592c5c836a1b8a7f2b761c2b2bd977b8bd68df0f
eminkerimov <eminkerimovdev@gmail.com>
2023-01-31 18:33:49 +0400

    added new structure

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -8,58 +8,103 @@ import "./Movie.scss";
 const Movie = () => {
   const id = useParams().id;
 
-  const details = useFetch(
-    `/${id}/credits?${API_KEY}&language=en-US`
-  );
+  const details = useFetch(`/${id}/credits?${API_KEY}&language=en-US`);
 
-  const { data, loading, error } = useFetch(
-    `${id}?${API_KEY}`
-  );
+  const { data, loading, error } = useFetch(`${id}?${API_KEY}`);
+
+  const videos = useFetch(`${id}/videos?${API_KEY}&language=en-US`);
 
   return (
-    <div
-      className="movie"
-      style={{
-        backgroundImage: `url(${
-          data?.backdrop_path ? IMG_API + data?.backdrop_path : Default
-        })`,
-      }}
-    >
-      <img
-        className="movie__image"
-        src={data?.poster_path ? IMG_API + data?.poster_path : Default}
-        alt={data?.title}
-      />
-      <div className="movie__info">
-        <h1>{data?.title + ` (${data?.release_date.substr(0,4)})`}</h1>
-        <p>
-          <b>Release date: </b>
-          {data?.release_date}
-        </p>
-        <p>
-          <b>Genres: </b>
-          {data?.genres?.map((e) => e.name + " ")}
-        </p>
-        <p>
-          <b>Budget: </b>
-          {data?.budget} $
-        </p>
-        <p>
-          <b>Revenue: </b>
-          {data?.revenue} $
-        </p>
-        <p>
-          <b>Tagline : </b>
-          {data?.tagline}
-        </p>
-        <h2>Overview</h2>
-        <p>{data?.overview}</p>
+    <div className="movie">
+      <div
+        className="movie__main"
+        style={{
+          backgroundImage: `url(${
+            data?.backdrop_path ? IMG_API + data?.backdrop_path : Default
+          })`,
+        }}
+      >
+        <div className="container">
+          <div className="movie__main__title">
+            <div className="movie__main__title__left">
+              <h1>{data?.title}</h1>
+              <span>{data?.release_date.substr(0, 4)}</span>
+            </div>
+            <div className="movie__main__title__right">
+              <div className="rating">
+                <span>Rating:</span>
+                <h2>
+                  <i class="fa-solid fa-star"></i>
+                  {data?.vote_average.toFixed(1)}
+                </h2>
+                {/* <h3>({data?.vote_count})</h3> */}
+              </div>
+              <div className="popularity">
+                <span>Popularity:</span>
+                <h2>
+                  <i class="fa-solid fa-arrow-up-short-wide"></i>
+                  {data?.popularity.toFixed(0)}
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="movie__main__info">
+            <img
+              className="movie__main__info__image"
+              src={data?.poster_path ? IMG_API + data?.poster_path : Default}
+              alt={data?.title}
+            />
+            <div className="movie__main__info__video">
+              <iframe
+                width="100%"
+                height="450"
+                src={`https://www.youtube.com/embed/${videos?.data?.results?.[0]?.key}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
+            <div className="movie__main__info__cast">
+              <h3>Top cast :</h3>
+              {details?.data?.cast?.map(
+                (e, index) =>
+                  index < 10 && (
+                    <>
+                      <p>{e.name}</p>
+                      {/* <img src={e.profile_path ? IMG_API + e.profile_path : Default} alt={e.cast_id} /> */}
+                    </>
+                  )
+              )}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="movie__cast">
-        <h3>Top cast :</h3>
-        {details?.data?.cast?.map((e,index)=>(
-            index < 10 && <p>{e.name}</p>
-          ))}
+      <div className="overview">
+        <div className="container">
+          <h2>Overview</h2>
+          <p>{data?.overview}</p>
+          <p>
+            <b>Release date: </b>
+            {data?.release_date}
+          </p>
+          <p>
+            <b>Genres: </b>
+            {data?.genres?.map((e) => e.name + " ")}
+          </p>
+          <p>
+            <b>Budget: </b>
+            {data?.budget} $
+          </p>
+          <p>
+            <b>Revenue: </b>
+            {data?.revenue} $
+          </p>
+          <p>
+            <b>Tagline : </b>
+            {data?.tagline}
+          </p>
+        </div>
       </div>
     </div>
   );
