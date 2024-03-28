e66214b289f1368494d8ac76903ff69ec6e8ab53
eminkerimov <eminkerimovdev@gmail.com>
2024-03-29 02:47:23 +0400

    styles changed

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,30 +2,42 @@ import React from "react";
 import "./Reviews.scss";
 
 const Reviews = (reviews) => {
-
   return (
     <div className="container">
-    <h1>User reviews: {reviews?.results?.length}</h1>
-    <div className="reviews">
-      {reviews?.results?.length ? (
-        reviews.results.map((result, index) => (
-        <div className="reviews__box" key={index} onClick={() => window.location.replace(result.url)}>
-            <div className="reviews__box__header">
-            <div className="reviews__box__header-section">
-                <i className="fa-solid fa-user-pen"></i>
-                {result.author}
+      <h1>
+        User reviews: <span className="round">{reviews?.results?.length}</span>
+      </h1>
+      <div className="reviews">
+        {reviews?.results?.length ? (
+          reviews.results.map((result, index) => (
+            <div
+              className="reviews__box"
+              key={index}
+              onClick={() => window.location.replace(result.url)}
+            >
+              <div className="reviews__box__header">
+                <div className="reviews__box__header-section">
+                  <i className="fa-solid fa-user-pen"></i>
+                  {result.author}
                 </div>
-            <div className="reviews__box__header-section">
-                <i className="fa-regular fa-calendar-days"></i>
-                {result.created_at.slice(0, 10) + " " + result.created_at.slice(11, 16)}
+                <div className="reviews__box__header-section">
+                  <i className="fa-regular fa-calendar-days"></i>
+                  {result.created_at.slice(0, 10) +
+                    " " +
+                    result.created_at.slice(11, 16)}
                 </div>
+                <div className="reviews__box__header-section">
+                <i class="fa-solid fa-star"></i>
+                  {result.author_details.rating}
                 </div>
-            <div className="reviews__box__content">{result.content}</div>
+              </div>
+              <div className="reviews__box__content">{result.content}</div>
             </div>
-        ))) : (
-        <div className="reviews__empty">No reviews yet...</div>
-      )}
-    </div>
+          ))
+        ) : (
+          <div className="reviews__empty">No reviews yet...</div>
+        )}
+      </div>
     </div>
   );
 };
